0950bc6fb6e80e94e0bc48efdae8a27146ad1782
Philip Chase <pbc@ufl.edu>
2018-01-05 17:00:06 -0500

    Add createProtocol to test

--- test.php
+++ test.php
@@ -11,3 +11,31 @@ print($result);
 
 // $result = $pbc->client->request('getProtocolSubjects', array('ProtocolNo' => 'OCR20002'));
 // print($pbc->prettify_object($result));
+
+// createProtocol
+$protocolNo = 'OCR21902';
+$input = array(
+    'ProtocolImportData' => array(
+        'ProtocolNo' => $protocolNo,
+        'IrbNo' => '',
+        'Library' => 'Academic Health Center',
+        'Department' => 'MD-CTSI',
+        'OrganizationalUnit' => 'Academic Health Center',
+        'Title' => 'Send Rx',
+        'ShortTitle' => 'Send Rx',
+        'ProtocolType' => 'Other',
+        'EstimatedCompletionDate' => '2018-07-01',
+    ),
+);
+
+$input = json_decode(json_encode($input));
+
+print("<p>createProtocol</p>");
+$result = $pbc->client->request('createProtocol', $input);
+print($pbc->prettify_object($result));
+
+$input = json_decode(json_encode(array('protocolNo' => $protocolNo)));
+
+print("<p>getProtocol</p>");
+$result = $pbc->client->request('getProtocol', $input);
+print($pbc->prettify_object($result));
